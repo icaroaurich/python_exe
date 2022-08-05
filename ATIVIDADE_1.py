152c8281baa16e75dd5f94b53b9d2c187ef98a30
SK <79989219+icaroaurich@users.noreply.github.com>
2022-08-04 21:23:07 -0300

    Delete ATIVIDADE_1.py

--- ATIVIDADE_1.py
+++
@@ -1,8 +0,0 @@
-#01 - Crie um programa que escreva "Olá Mundo" na tela;
-mensagem_inicial = "Olá Mundo"
-print(mensagem_inicial)
-
-## Por que uma variável com a mensagem e depois print da varável em vez de fazer um print diretamente com o texto? ##
-#1- O seu print pode existir em diferentes linhas do código, e assim você não precisa reescrever todas as vezess
-#2 - É interesante colocar todas as variáveis no inicio, para assim fácil modificação/manutenção, em vez de procurar em que linha está,
-# pode parece bobagem agora, mas quando seu código tiver muitas linhas, vai ser mais fácil.
