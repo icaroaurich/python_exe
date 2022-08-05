e4e215b3e11011b637d2b87e084763d89f0179ff
SK <79989219+icaroaurich@users.noreply.github.com>
2022-08-04 21:23:08 -0300

    Delete ATIVIDADE_2.py

--- ATIVIDADE_2.py
+++
@@ -1,8 +0,0 @@
-# 02 -Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas;
-xnome = "João Zinho, o brabo"
-print(xnome)
-
-## ou com input
-
-xnome_2 = input("Digite seu nome: ")
-print(xnome_2)
